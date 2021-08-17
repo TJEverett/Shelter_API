23d4bd24f242f565b59530a28cc5b37f9aeee6c4
Tristen Everett <spock214@gmail.com>
2021-08-17 15:36:05 -0700

    add Initial migration

---
+++ ShelterAPI/Migrations/20210817222956_Initial.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShelterAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cats",
+                columns: table => new
+                {
+                    CatId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    WeightKilo = table.Column<double>(nullable: false),
+                    IsFemale = table.Column<bool>(nullable: false),
+                    Birthday = table.Column<DateTime>(nullable: false),
+                    Coloring = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cats", x => x.CatId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dogs",
+                columns: table => new
+                {
+                    DogId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    WeightKilo = table.Column<double>(nullable: false),
+                    IsFemale = table.Column<bool>(nullable: false),
+                    Birthday = table.Column<DateTime>(nullable: false),
+                    Coloring = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Breed = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dogs", x => x.DogId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cats");
+
+            migrationBuilder.DropTable(
+                name: "Dogs");
+        }
+    }
+}
