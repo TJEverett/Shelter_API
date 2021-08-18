91a873885f7ac31ce3587992f2b07c96cddd0ea2
Tristen Everett <spock214@gmail.com>
2021-08-18 14:57:59 -0700

    add functionality to return list of cats in the database and add a new one to the database

---
+++ ShelterAPI/Controllers/CatsController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ShelterAPI.Models;
+
+namespace ShelterAPI.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class CatsController : ControllerBase
+  {
+    private AnimalShelterContext _db;
+
+    public CatsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/cat
+    [HttpGet]
+    public ActionResult<IEnumerable<Cat>> Get()
+    {
+      return _db.Cats.ToList();
+    }
+
+    [HttpPost]
+    public void Post([FromBody] Cat cat)
+    {
+      _db.Cats.Add(cat);
+      _db.SaveChanges();
+    }
+  }
+}
